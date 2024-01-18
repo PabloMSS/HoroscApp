abd27167544b005b56ab9f95f0e60fb04c23e54c
Pablo  Martin <pablomartinsanchezseco02@gmail.com>
2024-01-19 00:27:19 +0100

    IMG Horoscope y Model

--- app/src/main/java/com/example/horoscapp/domain/model/HoroscopeInfo.kt
+++ app/src/main/java/com/example/horoscapp/domain/model/HoroscopeInfo.kt
@@ -4,16 +4,16 @@ import com.example.horoscapp.R
 
 sealed class HoroscopeInfo(val img:Int, val name:Int){
     object Aries: HoroscopeInfo(R.drawable.aquario, R.string.aries)
-    object Taurus: HoroscopeInfo(R.drawable.aquario, R.string.taurus)
-    object Gemini: HoroscopeInfo(R.drawable.aquario, R.string.gemini)
-    object Cancer: HoroscopeInfo(R.drawable.aquario, R.string.cancer)
-    object Leo: HoroscopeInfo(R.drawable.aquario, R.string.leo)
-    object Virgo: HoroscopeInfo(R.drawable.aquario, R.string.virgo)
-    object Libra: HoroscopeInfo(R.drawable.aquario, R.string.libra)
-    object Scorpio: HoroscopeInfo(R.drawable.aquario, R.string.scorpio)
-    object Sagittarius: HoroscopeInfo(R.drawable.aquario, R.string.sagittarius)
-    object Capricorn: HoroscopeInfo(R.drawable.aquario, R.string.capricorn)
+    object Taurus: HoroscopeInfo(R.drawable.tauro, R.string.taurus)
+    object Gemini: HoroscopeInfo(R.drawable.geminis, R.string.gemini)
+    object Cancer: HoroscopeInfo(R.drawable.cancer, R.string.cancer)
+    object Leo: HoroscopeInfo(R.drawable.leo, R.string.leo)
+    object Virgo: HoroscopeInfo(R.drawable.virgo, R.string.virgo)
+    object Libra: HoroscopeInfo(R.drawable.libra, R.string.libra)
+    object Scorpio: HoroscopeInfo(R.drawable.escorpio, R.string.scorpio)
+    object Sagittarius: HoroscopeInfo(R.drawable.sagitario, R.string.sagittarius)
+    object Capricorn: HoroscopeInfo(R.drawable.capricornio, R.string.capricorn)
     object Aquarius: HoroscopeInfo(R.drawable.aquario, R.string.aquarius)
-    object Pisces: HoroscopeInfo(R.drawable.aquario, R.string.pisces)
+    object Pisces: HoroscopeInfo(R.drawable.piscis, R.string.pisces)
 
 }
